5d4363d6129aae928efd84bd7ecef8763b002d6d
Dalila Pereira <dalilajp@ymail.com>
2021-06-13 16:29:43 -0300

    Inclusão Cucumber-JVM Report

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features",
-        glue = {"steps"}, monochrome = true, dryRun = false)
+        glue = {"steps"}, monochrome = true, dryRun = false,
+        plugin = {"json:target/cucumber.json"})
 public class Runner {
 
 }
